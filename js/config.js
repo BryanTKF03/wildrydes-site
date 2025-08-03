1a5606a2dba023cd0b291033722e830734b923fa
BryanTKF03 <Bryantham767@gmail.com>
2025-08-04 03:21:36 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-southeast-1_2hoEBAZmA', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3l8nua8b6uonggdsqvek29jhgk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-southeast-1' // e.g. us-east-2
+        userPoolId: 'ap-southeast-1_2hoEBAZmA',
+        userPoolClientId: '3l8nua8b6uonggdsqvek29jhgk',
+        region: 'ap-southeast-1'
     },
     api: {
-        invokeUrl: 'https://bw5qubnnm5.execute-api.ap-southeast-1.amazonaws.com/devRydes' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://bw5qubnnm5.execute-api.ap-southeast-1.amazonaws.com/devRydes'
     }
 };
+
 
 
 
